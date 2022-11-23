897bf4be1f40acab80faa70e736dd41d9d0bd844
rommelmamedov <rommelmamedov@gmail.com>
2022-11-23 19:44:46 +0400

    footer added

--- components/Header.js
+++ components/Header.js
@@ -14,22 +14,12 @@ export const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className="container">
-				<div className={styles.wrapper}>
+				<nav className={styles.wrapper}>
 					<Link href="/">
 						<Image src={logo} alt="Voiceloft Logo" width={200} height={60} priority />
 					</Link>
-					<nav>
-						<ul className={styles.list}>
-							{navigationItems.map(({ title, href }) => (
-								<li key={href}>
-									<Link className={styles.link} href={href} title={title}>
-										{title}
-									</Link>
-								</li>
-							))}
-						</ul>
-					</nav>
-				</div>
+					<button>Schedule a call</button>
+				</nav>
 			</div>
 		</header>
 	);
