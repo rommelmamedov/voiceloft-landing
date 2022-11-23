b02c7827ec29b1862d0a081adefbcb8f8076bc3b
rommelmamedov <rommelmamedov@gmail.com>
2022-11-23 17:45:40 +0400

    facebook pixel integrated

---
+++ libs/facebook-pixel.js
@@ -0,0 +1,6 @@
+export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+
+export const pageView = () => window.fbq('track', 'PageView');
+
+// https://developers.facebook.com/docs/facebook-pixel/advanced/
+export const event = (name, options = {}) => window.fbq('track', name, options);
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,23 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { FB_PIXEL_ID } from '@libs/facebook-pixel';
+
 const Document = () => {
 	return (
 		<Html lang="en">
 			<Head>
 				<meta charSet="UTF-8" />
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<noscript>
+					{/* eslint-disable-next-line @next/next/no-img-element */}
+					<img
+						height="1"
+						width="1"
+						alt="fb-pixel"
+						style={{ display: 'none' }}
+						src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+					/>
+				</noscript>
 			</Head>
 			<body>
 				<Main />
