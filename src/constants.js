6355e333b81746fb31deebf291265261fca0e1a4
rommelmamedov <rommelmamedov@gmail.com>
2022-11-29 01:11:20 +0400

    voice section fixes

--- src/constants.js
+++ src/constants.js
@@ -1,35 +1,43 @@
+import gozle from '@sounds/ovcu.mp3';
+
 export const voices = [
 	{
+		id: '1',
 		avatar: '',
-		audio: '',
+		audio: gozle,
 		style: { backgroundColor: '#a7fbec', color: '#03b998' },
 		text: 'Welcome, can I get your order?',
 	},
 	{
+		id: '2',
 		avatar: '',
 		audio: '',
 		style: { backgroundColor: '#ffd9d9', color: '#d17272' },
 		text: 'Hi, Can I get a chicken sandwich?',
 	},
 	{
+		id: '3',
 		avatar: '',
 		audio: '',
 		style: { backgroundColor: '#e8f0be', color: '#f19b5f' },
 		text: 'Any drink with that?',
 	},
 	{
+		id: '4',
 		avatar: '',
 		audio: '',
 		style: { backgroundColor: '#ccf3ff', color: '#272ffb' },
 		text: 'A bottle of water, please.',
 	},
 	{
+		id: '5',
 		avatar: '',
 		audio: '',
 		style: { backgroundColor: '#a7fbec', color: '#03b998' },
 		text: 'Let me repeat your order: a chicken sandwich and a bottle of water.',
 	},
 	{
+		id: '6',
 		avatar: '',
 		audio: '',
 		style: { backgroundColor: '#ffd9d9', color: '#d17272' },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 
 import { Layout } from '@components/Layout';
+import { VoiceButton } from '@components/VoiceButton';
 
 // import VoiceUploader from '@components/VoiceUploader';
 import speechIdentification from '@images/persons.webp';
 
+import { triangle, voice } from '@styles/components/VoiceButton.module.css';
 import styles from '@styles/pages/Home.module.css';
 
 import { voices } from '../constants';
@@ -29,15 +31,20 @@ const Home = () => {
 				<div className="container">
 					<div className="section-wrapper-flex">
 						<div className={styles.voices}>
-							{voices.map(({ avatar, audio, text, style }, index) => (
-								<button key={index} className={styles.voice} style={style}>
-									<svg width="36" height="36" viewBox="0 0 24 24">
-										<path d="M16 21c3.527-1.547 5.999-4.909 5.999-9S19.527 4.547 16 3v2c2.387 1.386 3.999 4.047 3.999 7S18.387 17.614 16 19v2z" />
-										<path d="M16 7v10c1.225-1.1 2-3.229 2-5s-.775-3.9-2-5zM4 17h2.697L14 21.868V2.132L6.697 7H4c-1.103 0-2 .897-2 2v6c0 1.103.897 2 2 2z" />
-									</svg>
-									{text}
-								</button>
+							{voices.map(voiceProps => (
+								<VoiceButton key={voiceProps.id} {...voiceProps} />
 							))}
+							<div className={voice} style={{ backgroundColor: '#e8f0be', cursor: 'default' }}>
+								<div className="ellipsis">
+									<div></div>
+									<div></div>
+									<div></div>
+									<div></div>
+								</div>
+								<svg className={triangle} width={24} height={18} viewBox="0 0 24 18">
+									<path d="M.845.265h22.403l-2.425 1.043A45.894 45.894 0 0 0 .845 17.901V.265z" fill="#e8f0be" />
+								</svg>
+							</div>
 						</div>
 						<div className={styles.description}>
 							<h2>Speech to text</h2>
